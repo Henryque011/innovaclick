8c3ca51fc050eaacbc3a00a7f36a2ac59ec3000f
henryque nonato <henryquenonatosilva@gmail.com>
2024-12-06 11:43:21 -0300

    Organização dos arquivos

--- conteudo/banner.php
+++
@@ -1,9 +0,0 @@
-<section class="banner animate__animated animate__rubberBand">
-        <div>
-            <img src="img/banner1.png" alt="Banner 01">
-            <img src="img/banner1.png" alt="Banner 02">
-            <img src="img/banner1.png" alt="Banner 03">
-            <img src="img/banner1.png" alt="Banner 04">
-        </div>
-        <h2>INNOVA CLICK</h2>
-</section>
