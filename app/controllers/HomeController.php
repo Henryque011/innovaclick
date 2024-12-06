f192a472e7e59b521923b2130662f4b4f58a3e11
henryque nonato <henryquenonatosilva@gmail.com>
2024-12-06 11:47:31 -0300

    comment

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller{
         $dados['nome'] = 'Henryque';
 
         //var_dump($dados);
-
+        // clen code
         $this->carregarViews('home',$dados);
         
 
